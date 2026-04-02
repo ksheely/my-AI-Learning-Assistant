74316d7630059168b0e3a9a70e7aaebe9638cfac
ksheely <ksheely@mac.com>
2026-04-01 21:05:39 -0500

    First

--- main.py
+++ main.py
@@ -22,8 +22,11 @@ def get_openai_client():
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
 
 @app.post("/analyze")
 async def analyze(data: InputData):
